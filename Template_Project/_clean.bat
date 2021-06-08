137f51a102fc7e0ff5e0c298419afba59f25ef20
Yasin Calli <yasin-93@hotmail.de>
2021-06-08 21:42:08 +0200

    lol

---
+++ Template_Project/_clean.bat
@@ -0,0 +1,10 @@
+rmdir /S /Q Bin\Virtual
+rmdir /S /Q Bin\LPC17xx
+rmdir /S /Q Bin\STM32L1xx
+rmdir /S /Q Bin
+rmdir /S /Q DebugConfig
+
+del *.uvgui.* *.uvguix.* *.bak *.dep *.depend main.exe *.scvd "Target 1.bat"
+
+
+
