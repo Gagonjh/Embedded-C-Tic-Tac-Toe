393e98f765a3ddab2fae94e43e45773fbe3becb3
Joshua <joshua.hahn@smail.inf.h-brs.de>
2021-06-08 20:09:30 +0200

    Initialer Commit

---
+++ Template/_clean.bat
@@ -0,0 +1,10 @@
+rmdir /S /Q Bin\Virtual
+rmdir /S /Q Bin\LPC17xx
+rmdir /S /Q Bin\STM32L1xx
+rmdir /S /Q Bin
+rmdir /S /Q DebugConfig
+
+del *.scvd *.uvgui.* *.uvguix.* *.bak *.dep *.depend main.exe *.layout "Target 1.bat"
+
+
+
