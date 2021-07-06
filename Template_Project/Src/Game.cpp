c26fe4f6b5ad4706d1b14a59578b9eaa75ee3a24
InFlameOus171 <49519354+InFlameOus171@users.noreply.github.com>
2021-07-06 12:09:01 +0200

    update game

--- Template_Project/Src/Game.cpp
+++ Template_Project/Src/Game.cpp
@@ -5,6 +5,12 @@ Game::Game(BYTE gameMode)
 	gameMode = gameMode;
 }
 
+void Game::newGame()
+{
+	Controller controller;
+	this->controller = controller;
+}
+
 short Game::ttt_classic(short posX,short posY)
 {
 	if(posX<390 && posX > 100 && posY < 390 && posY > 100) 
--- Template_Project/Src/Game.h
+++ Template_Project/Src/Game.h
@@ -7,6 +7,7 @@ class Game
 		Controller controller;
 		BYTE gameMode;
 	public:
+		void newGame();
 		short ttt_classic(short,short);
 		Game(BYTE);
 };
